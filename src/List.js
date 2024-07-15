f799c6b024f16076a0a03c1e272c4e9871f4f592
Ismailov <Iismailov@hospiceatcharlotte.net>
2024-07-15 16:43:06 -0400

    cleaned up the code a bit as i had no idea what i was doing back in the day lol

--- src/List.js
+++ src/List.js
@@ -3,8 +3,6 @@ import "./List.css";
 
 function List({ getRemaining }) {
   const [list, setList] = useState([]);
-  const [doneList, setDoneList] = useState([]);
-  const [newTask, setNewTask] = useState("");
 
   var counter = 0;
   list.filter((item) => {
@@ -17,34 +15,36 @@ function List({ getRemaining }) {
     getRemaining(counter);
   });
 
-  ////////////////////////////////////// This area fetches the Tasks
-  useEffect(() => {
-    async function getRecords() {
-      const response = await fetch(`http://localhost:8080/api/v1/tasks`);
+  async function getRecords() {
+    const response = await fetch(`http://localhost:8080/api/v1/tasks`);
 
-      if (!response.ok) {
-        const message = `An error occurred: ${response.statusText}`;
-        window.alert(message);
-        return;
-      }
-
-      const records = await response.json();
-      setList(records);
+    if (!response.ok) {
+      const message = `An error occurred: ${response.statusText}`;
+      window.alert(message);
+      return;
     }
 
+    const records = await response.json();
+    setList(records);
+  }
+
+  ////////////////////////////////////// This area fetches the Tasks
+  useEffect(() => {
     getRecords();
 
     return;
-  }, [list.length]);
+  }, []);
 
   /////////////////////////////////// This is the handler for creating a task
   async function handleSubmit(e) {
     e.preventDefault();
+    const newTask = document.getElementById("newTask").value;
+    console.log("Adding: ", newTask);
     if (newTask.length > 0) {
       const toPost = {
         description: newTask,
         completed: false,
-        created: "1987-04-02",
+        created: Date(),
       };
       const response = await fetch("http://localhost:8080/api/v1/tasks", {
         method: "POST",
@@ -58,55 +58,33 @@ function List({ getRemaining }) {
         console.log("It worked");
       }
     }
-
-    setNewTask("");
-    if (list.length > 1) {
-      setList([]);
-    } else {
-      setList(["testing", "this"]);
-    }
+    getRecords();
   }
   //////////////////////////////////// This is the handler for when you delete a task
   async function handleDelete(val) {
-    console.log("Request delete for: " + val._id);
-    const res = await fetch(`http://localhost:3002/${val._id}`, {
+    console.log("Request delete for: " + val);
+    const res = await fetch(`http://localhost:8080/api/v1/tasks/${val}`, {
       method: "DELETE",
     });
-    const result = list.filter((item) => {
-      return item._id !== val._id;
-    });
-    if (res) {
-      setList([]);
-    }
+
+    getRecords();
   }
   //////////////////////////////////// This is the handler for when you check a task off
   async function handleComplete(val) {
-    setDoneList([...doneList], val);
+    console.log("Requesting status change for: " + val.description);
 
-    console.log("Requesting status change for: " + JSON.stringify(val));
-
-    const toPatch = {
-      description: val.task,
-      completed: !val.completed,
-    };
-
-    console.log("To patch completed" + toPatch.completed);
-
-    const response = await fetch(`http://localhost:3002/${val._id}`, {
-      method: "PATCH",
-      body: JSON.stringify(toPatch),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response) {
-      if (list.length > 1) {
-        setList([]);
-      } else {
-        setList(["testing", "this"]);
+    const response = await fetch(
+      `http://localhost:8080/api/v1/tasks/${
+        val.id
+      }?completed=${!val.completed}`,
+      {
+        method: "PUT",
       }
-    }
+    );
+
+    console.log("response: ", response);
+
+    getRecords();
   }
 
   console.log(list);
@@ -114,7 +92,7 @@ function List({ getRemaining }) {
   const mappedItems = list.map((item) => {
     return (
       <li
-        key={list.indexOf(item)}
+        key={item.id}
         className={
           item.completed ? "task-container-completed" : "task-container"
         }
@@ -124,7 +102,7 @@ function List({ getRemaining }) {
             className="checkbox"
             type="checkbox"
             defaultChecked={item.completed}
-            onChange={(e) => {
+            onChange={() => {
               handleComplete(item);
             }}
           />
@@ -136,7 +114,7 @@ function List({ getRemaining }) {
           <button
             className="del-btn"
             onClick={() => {
-              handleDelete(item);
+              handleDelete(item.id);
             }}
           >
             X
@@ -149,11 +127,7 @@ function List({ getRemaining }) {
   return (
     <>
       <form onSubmit={handleSubmit} className="new-task-field">
-        <input
-          className={"new-task-text"}
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-        />
+        <input className={"new-task-text"} id="newTask" />
         <button className={"new-task-button"} type="submit">
           +
         </button>
